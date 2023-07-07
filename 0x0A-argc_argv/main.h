78ecd976ab0592c379b3394072ad66fb619e4d83
Belkharfa <belkharfa.saad@gmail.com>
2023-07-07 16:59:53 -0700

    task0123

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+int is_number(char *s);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int make_change(int n);
+int main(int argc, char *argv[]);
+int main(int argc, __attribute__((unused)) char *argv[]);
+int main(__attribute__((unused)) int argc, char *argv[]);
+int make_change(int n);
+int main(int argc, char *argv[]);
+
