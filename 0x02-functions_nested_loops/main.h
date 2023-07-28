c10d2a1823c9f235ade13c7b8a7b74392e3483b7
belkharfa <belkharfa.saad@gmail.com>
2023-07-28 15:03:32 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,35 @@
-#include "holberton.h"
+#ifndef MAIN_H_INCLUDED
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+#define MAIN_H_INCLUDED
+
+
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+
+#endif
