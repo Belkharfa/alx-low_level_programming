0e22f3de518873b1b1afb9c3329117c679defc3d
belkharfa <belkharfa.saad@gmail.com>
2023-08-07 15:44:41 -0700

    task 12345

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,61 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
+ * _strlen - returns length of string (modified)
+ * @s: string (const)
+ * Return: length of string
  */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
+ */
+
+int power(int base, int exp)
+{
+	int i, num;
+
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+
+	return (num);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0;
-	int len, i;
-	int decval = 1;
+	unsigned int sum;
+	int length, i;
 
-	if (!b)
-		return (0);
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	i = (len - 1);
-	for (; i >= 0; i--)
+	sum = 0;
+	if (b == NULL)
+		return (sum);
+	length = _strlen(b);
+	for (i = length - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		if (b[i] == '1')
-			total += decval;
-		decval *= 2;
+		sum += (b[i] - '0') * power(2, length - i - 1);
 	}
-
-	return (total);
+	return (sum);
 }
